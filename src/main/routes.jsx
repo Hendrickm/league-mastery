18a29051c4a9f04f05ab9cb547a9e4cf173ce963
hendrickm <hendrick.lima@gmail.com>
2020-02-06 20:04:24 -0300

    Adiciona rotas principais

---
+++ src/main/routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Profile from '../profile/profile'
+import Home from '../home/home'
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  </BrowserRouter>
+)
