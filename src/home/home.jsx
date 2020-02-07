b6e1dd301cb6a7d83f6528c5e3c7d2353a590d9c
hendrickm <hendrick.lima@gmail.com>
2020-02-06 21:26:04 -0300

    Adiciona componente inputSearch

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 
 import './home.css'
+import InputSearch from '../components/inputSearch'
 
 
 export default class Home extends Component {
 
+
+  handleClickSearch = search => {
+    console.log(search);
+  }
+  
   render() {
     return (
-      <div> 
-        Home
+      <div className="input-search"> 
+        <h1>League Mastery</h1>
+        <InputSearch handleClickSearch={this.handleClickSearch} />
       </div>
     )
   }
