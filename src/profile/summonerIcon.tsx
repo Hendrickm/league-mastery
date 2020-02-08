8eaaae0d426e1662e36a3e1461310a366a616a64
hendrickm <hendrick.lima@gmail.com>
2020-02-08 18:00:14 -0300

    Adiciona componente de icone

---
+++ src/profile/summonerIcon.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface Props {
+  profileIconId: number,
+  summonerLevel: number,
+}
+
+export default (props: Props) => {
+  const { profileIconId, summonerLevel } = props;
+
+  const getProfileIcon = () => (
+    <img
+      className="summoner-icon"
+      src={`http://ddragon.leagueoflegends.com/cdn/10.3.1/img/profileicon/${profileIconId}.png`}
+      alt="icone"
+    />
+  );
+
+  return (
+
+    <div className="icon-container">
+      {getProfileIcon()}
+      <div className="summoner-level">{summonerLevel}</div>
+    </div>
+  );
+};
