ae923cb022467100601b1535a564906645e106d8
hendrickm <hendrick.lima@gmail.com>
2020-02-10 19:00:36 -0300

    Ajuste CSS

--- src/profile/championIcon.tsx
+++ src/profile/championIcon.tsx
@@ -11,19 +11,10 @@ interface Props {
 export default (props: Props) => {
   const { champion: { championId, championPoints, championLevel } } = props;
 
-  const getChampionClass = () => {
-    let classes = 'champion';
-    if (championLevel === 7) classes += ' m7';
-    if (championLevel === 6) classes += ' m6';
-    if (championLevel === 5) classes += ' m5';
-    if (championLevel === 4) classes += ' m4';
-    return classes;
-  };
-
 
   return (
     <div key={championId} className="champion-container">
-      <div className={`${getChampionClass()}`}>
+      <div className="champion">
         <div className="champion-name">{getChampionName(championId)}</div>
         <img className="champion-icon" src={`http://ddragon.leagueoflegends.com/cdn/10.3.1/img/champion/${getChampionKey(championId)}.png`} alt={championId.toString()} />
         <div className="champion-name">{championPoints}</div>
